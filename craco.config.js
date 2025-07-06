829582eaa19718cf84d36de9cacdc1fb68262d72
Kaushik Roy Chowdhury <kaushikrch@gmail.com>
2025-07-07 00:06:40 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,18 @@
 // craco.config.js
 const path = require("path");
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@/components": path.resolve(__dirname, "src/components"),
-    },
+      "@":           path.resolve(__dirname, "src"),
+      "@/components": path.resolve(__dirname, "src/components")
+    }
   },
   babel: {
+    // Only ever include react-refresh/babel once, and only in dev
     plugins: [
-      // Force React-Refresh plugin to load even in production
-      ["react-refresh/babel", { skipEnvCheck: true }],
-    ],
-  },
+      isDev && require.resolve("react-refresh/babel")
+    ].filter(Boolean)
+  }
 };
