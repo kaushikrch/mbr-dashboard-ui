54a43ed0cdc9ef35e3521b2e77d7cf14925ab695
Kaushik Roy Chowdhury <kaushikrch@gmail.com>
2025-07-06 19:11:04 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import App from './App';         // default import
-import './index.css';
+// main.tsx (or index.tsx)
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    {/* pass defaultSystem here so TypeScript is happy */}
+    <ChakraProvider value={defaultSystem}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
