323c272b6315f21164f03f74d24e49d11ea07b1e
Kaushik Roy Chowdhury <kaushikrch@gmail.com>
2025-07-06 18:42:03 +0530

    fix: add default export for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import App from './App';
-import './index.css';
+import React from "react";
+import PredictiveAnalytics from "./components/PredictiveAnalytics";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+const App: React.FC = () => {
+  return <PredictiveAnalytics />;
+};
+
+export default App;
